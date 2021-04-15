41ac64fddf8926135df3606a1ad3b8cd58336779
Anshul Asawa <35421635+TheAnshul756@users.noreply.github.com>
2021-04-15 22:08:06 +0530

    initial commit

---
+++ generator.cpp
@@ -0,0 +1,41 @@
+#include "testlib.h"
+#include<bits/stdc++.h>
+#define ll          long long
+#define pb          push_back
+#define ppb         pop_back
+#define endl        '\n'
+#define mii         map<ll int,ll int>
+#define msi         map<string,ll int>
+#define mis         map<ll int, string>
+#define rep(i,a,b)    for(ll int i=a;i<b;i++)
+#define mpi         map<pair<ll int,ll int>,ll int>
+#define pii         pair<ll int,ll int>
+#define vi          vector<ll int>
+#define vii         vector<pair<ll int, ll int>>
+#define vs          vector<string>
+#define all(a)      (a).begin(),(a).end()
+#define F           first
+#define S           second
+#define sz(x)       (ll int)x.size()
+#define hell        1000000007
+using namespace std;
+
+
+void writeTest(int test)
+{
+    startTest(test);
+    /*
+        Generator Code
+    */
+    
+    return; 
+}
+
+
+int main(int argc, char* argv[])
+{
+    int tests = atoi(argv[1]);
+    for (int i = 0; i < tests; i++)
+        writeTest(i);
+    return 0;
+}
